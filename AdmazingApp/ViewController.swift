8a076f37d90279872ed94afe081f5ca1643e23ac
cnpm18 <cnpm18@hotmail.com>
2016-06-20 14:15:43 -0500

    Initial Commit

---
+++ AdmazingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdmazingApp
+//
+//  Created by Cristopher Nunez Del Prado on 20/06/16.
+//  Copyright © 2016 Cristopher Nunez Del Prado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AdmazingAppTests/AdmazingAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  AdmazingAppTests.swift
+//  AdmazingAppTests
+//
+//  Created by Cristopher Nunez Del Prado on 20/06/16.
+//  Copyright © 2016 Cristopher Nunez Del Prado. All rights reserved.
+//
+
+import XCTest
+@testable import AdmazingApp
+
+class AdmazingAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
